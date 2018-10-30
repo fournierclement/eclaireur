9fedab43bd300aef7b99157552fc0ce85e578bc6
Fournier Clement <fournier.clt@gmail.com>
2018-10-30 09:37:21 +0100

    add help, cancel and stop intend

--- app/NewCharacterState.js
+++ app/NewCharacterState.js
@@ -8,7 +8,23 @@ module.exports = {
     this.followUpState("OpenedCharacter")
       .ask(`Le personnage de ${character_name} vient d'être ajouté à votre collection. ${character.promptTODO}`)
   },
+  "NoIntent": function(){
+    this.toIntent("AMAZON.CancelIntent");
+  },
   'Unhandled': function(){
-    this.ask("ok")
-  }
+    this.toIntent("AMAZON.HelpIntent");
+  },
+  "AMAZON.HelpIntent": function () {
+    let character_name = this.getSessionAttribute('character_name');
+    this.followUpState(this.getState())
+    .ask(`Voulez-vous créer le personnage de ${character_name} ?`)
+  },
+  "AMAZON.CancelIntent": function () {
+    this.setSessionAttribute("character_name", undefined);
+    this.followUpState(null)
+    .toIntent("Unhandled");
+  },
+  "AMAZON.StopIntent": function () {
+    this.tell("Création de personnage annulée, bonne journée.")
+  },
 }
